dd6fb16421cf247973e48895c1073474ab84f255
AlgoPrashant <prashant.verma@algorisys.com>
2024-05-15 09:19:07 +0530

    pass 8th stage

--- src/Server.cs
+++ src/Server.cs
@@ -1,103 +1,102 @@
+using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.IO;
 
-byte[] generateResponse(string status, string contentType, string responseBody) {
-    // Status Line
-    string response = $"HTTP/1.1 {status}\r\n";
+class Program
+{
+    static byte[] generateResponse(string status, string contentType, string responseBody)
+    {
+        // Status Line
+        string response = $"HTTP/1.1 {status}\r\n";
 
-    // Headers
-    response += $"Content-Type: {contentType}\r\n";
-    response += $"Content-Length: {responseBody.Length}\r\n";
-    response += "\r\n";
+        // Headers
+        response += $"Content-Type: {contentType}\r\n";
+        response += $"Content-Length: {responseBody.Length}\r\n";
+        response += "\r\n";
 
-    // Response Body
-    response += responseBody;
+        // Response Body
+        response += responseBody;
 
-    return Encoding.UTF8.GetBytes(response);
-}
-
-string readFile(string filepath) {
-    // Handling exceptions "higher" up
-    string fileContents = "";
-
-    // Read file using stream
-    StreamReader fp = new StreamReader(filepath);
-    var line = fp.ReadLine();
-
-    while (line != null) {
-        fileContents += line;
-        line = fp.ReadLine();
-    }
-    fp.Close();
-
-    return fileContents;
-}
-
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
-// Create TcpListener
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-
-while (true) {
-    // Create new socket
-    Socket client =  server.AcceptSocket();
-    Console.WriteLine("Connection Established");
-
-    // Create response buffer and get resonse
-    byte[] requestText = new byte[100];
-    client.Receive(requestText);
-
-    // Parse request path
-    string parsed = System.Text.Encoding.UTF8.GetString(requestText);
-    string[] parsedLines = parsed.Split("\r\n");
-
-    // Capturing specific parts of the request that will always be there
-    string method = parsedLines[0].Split(" ")[0]; // GET, POST
-    string path = parsedLines[0].Split(" ")[1]; // /echo/apple
-
-    // Logic
-    if (path.Equals("/")) {
-        client.Send(generateResponse("200 OK", "text/plain", "Nothing"));
+        return Encoding.UTF8.GetBytes(response);
     }
 
-    // Return if file specified after '/files/' exists, return contents in resonse body
-    else if (path.StartsWith("/files/")) {
-        // Instructions mention the program WILL be run like this ./program --directory dir
-        string directoryName = args[1];
-        string filename = path.Split("/")[2];
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Logs from your program will appear here!");
 
-        try {
-            string fileContents = readFile(directoryName + filename);
-            client.Send(generateResponse("200 OK", "application/octet-stream", fileContents));
+        // Ensure correct command line arguments
+        if (args.Length < 2 || args[0] != "--directory")
+        {
+            Console.WriteLine("Usage: ./your_server.sh --directory <directory>");
+            return;
         }
-        catch (Exception e){
-            client.Send(generateResponse("404 Not Found", "text/plain", "File Not Found"));
+
+        // Create TcpListener
+        TcpListener server = new TcpListener(IPAddress.Any, 4221);
+        server.Start();
+
+        while (true)
+        {
+            // Create new socket
+            Socket client = server.AcceptSocket();
+            Console.WriteLine("Connection Established");
+
+            // Create response buffer and get response
+            byte[] requestText = new byte[100];
+            client.Receive(requestText);
+
+            // Parse request path
+            string parsed = Encoding.UTF8.GetString(requestText);
+            string[] parsedLines = parsed.Split("\r\n");
+
+            // Capturing specific parts of the request that will always be there
+            string method = parsedLines[0].Split(" ")[0]; // GET, POST
+            string path = parsedLines[0].Split(" ")[1]; // /echo/apple
+
+            // Logic
+            if (path.Equals("/"))
+            {
+                client.Send(generateResponse("200 OK", "text/plain", "Nothing"));
+            }
+            else if (path.StartsWith("/files/"))
+            {
+                // Extract filename from the request path
+                string filename = path.Split("/")[2];
+                
+                // Read the contents of the request body
+                int contentLength = int.Parse(parsedLines[1].Split(" ")[1]);
+                byte[] requestBody = new byte[contentLength];
+                client.Receive(requestBody);
+                string fileContents = Encoding.UTF8.GetString(requestBody);
+                
+                // Save the file contents to the specified directory
+                string directoryName = args[1];
+                string filePath = Path.Combine(directoryName, filename);
+                File.WriteAllText(filePath, fileContents);
+                
+                // Send a response with status code 201 (Created)
+                client.Send(generateResponse("201 Created", "text/plain", "File created successfully"));
+            }
+            else if (path.Equals("/user-agent"))
+            {
+                string userAgent = parsedLines[2].Split(" ")[1];
+                client.Send(generateResponse("200 OK", "text/plain", userAgent));
+            }
+            else if (path.StartsWith("/echo"))
+            {
+                string word = path.Split("/")[2];
+                client.Send(generateResponse("200 OK", "text/plain", word));
+            }
+            else
+            {
+                client.Send(generateResponse("404 Not Found", "text/plain", "Nothing Dipshit"));
+            }
+
+            client.Close();
         }
-    }
 
-    // Return User-Agent in resonse body
-    else if (path.Equals("/user-agent")) {
-        string userAgent = parsedLines[2].Split(" ")[1];
-        client.Send(generateResponse("200 OK", "text/plain", userAgent));
+        //server.Stop(); // This line is unreachable since the loop is infinite
     }
-
-    // Return text after '/echo/' in resonse body
-    else if (path.StartsWith("/echo")) {
-        string word = path.Split("/")[2];
-        client.Send(generateResponse("200 OK", "text/plain", word));
-    }
-
-    // You're a loser
-    else {
-        client.Send(generateResponse("404 Not Found", "text/plain", "Nothing Dipshit"));
-    }
-
-    client.Close();
 }
-
-server.Stop();
